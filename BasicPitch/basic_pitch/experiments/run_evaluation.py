0ba6a2420cb631b083226fbeb6e4f62a90240bd3
Lorenzo Giare <gialollog@gmail.com>
2024-08-20 12:18:20 +0200

    added spotify-hwd model

--- BasicPitch/basic_pitch/experiments/run_evaluation.py
+++ BasicPitch/basic_pitch/experiments/run_evaluation.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-m",
         "--model",
-        default=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "My_models/model.best/"),
+        default=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "My_models/spotify-hwd/model.best/"),
         type=str,
         help="Path to the saved model directory.",
     )
